db8f3c22dd67e07620c7b5f183ef3af932c90e67
Evan Mastrantuono <xnaveman@gmail.com>
2025-01-30 12:52:05 +0100

    thematique update button

--- api/thematiques/update.php
+++ api/thematiques/update.php
@@ -1,2 +1,11 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
+require_once '../../functions/query/update.php';
 
+$numThem = ctrlSaisies($_POST['numThem']);
+$libThem = ctrlSaisies($_POST['libThem']);
+
+sql_update('THEMATIQUE', "libThem = '$libThem'", "numThem = $numThem");
+
+header('Location: ../../views/backend/thematiques/list.php');
