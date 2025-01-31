d73fbc09edd877d8cccb3497ddfc01f9e3225762
Val LOMBART <33480.val@gmail.com>
2025-01-31 17:37:23 +0100

    Add member creation functionality and update member list view

--- api/members/create.php
+++ api/members/create.php
@@ -1,2 +1,14 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
 
+$pseudoMemb = ctrlSaisies($_POST['pseudoMemb']);
+$nomMemb = ctrlSaisies($_POST['nomMemb']);
+$prenomMemb = ctrlSaisies($_POST['prenomMemb']);
+$passMemb = ctrlSaisies($_POST['passMemb']);
+$eMailMemb = ctrlSaisies($_POST['eMailMemb']);
+
+sql_insert('MEMBRE', 'pseudoMemb, nomMemb, prenomMemb, passMemb, eMailMemb, numStat', "'$pseudoMemb', '$nomMemb', '$prenomMemb', '$passMemb', '$eMailMemb', '3' ");
+
+
+header('Location: ../../views/backend/members/list.php');
--- views/backend/members/create.php
+++ views/backend/members/create.php
@@ -1,3 +1,43 @@
 <?php
 include '../../../header.php';
+?>
 
+<!-- Bootstrap form to create a new statut -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Création nouveau Membre</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to create a new statut -->
+            <form action="<?php echo ROOT_URL . '/api/members/create.php' ?>" method="post">
+                <div class="form-group">
+                    <label for="pseudoMemb">Pseudo du membre</label>
+                    <input id="pseudoMemb" name="pseudoMemb" class="form-control" type="text" autofocus="autofocus" />
+                </div>
+                <div class="form-group">
+                    <label for="nomMemb">Nom du membre</label>
+                    <input id="nomMemb" name="nomMemb" class="form-control" type="text" autofocus="autofocus" />
+                </div>
+                <div class="form-group">
+                    <label for="prenomMemb">Prénom du membre</label>
+                    <input id="prenomMemb" name="prenomMemb" class="form-control" type="text" autofocus="autofocus" />
+                </div>
+                <div class="form-group">
+                    <label for="passMemb">Mot de passe du membre</label>
+                    <input id="passMemb" name="passMemb" class="form-control" type="text" autofocus="autofocus" />
+                </div>
+                <div class="form-group">
+                    <label for="eMailMemb">Mail du membre</label>
+                    <input id="eMailMemb" name="eMailMemb" class="form-control" type="text" autofocus="autofocus" />
+                </div>
+                <br/>
+                <div class="form-group mt-2">
+                    <a href="list.php" class="btn btn-primary">List</a>
+                    <button type="submit" class="btn btn-success">Confirmer create ?</button>
+                </div>
+                
+            </form>
+        </div>
+    </div>
+</div>
