c3fceea6ce3107f5b7854f5f75d304df0103fdcd
EricTrky <erictranky@gmail.com>
2025-02-04 16:56:55 +0100

    Comments Delete + Update + List(V2)

--- api/comments/delete.php
+++ api/comments/delete.php
@@ -1,2 +1,10 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
 
+$numCom = ctrlSaisies($_POST['numCom']);
+
+sql_delete('comment', "numCom= $numCom");
+
+
+header('Location: ../../views/backend/comments/list.php');
