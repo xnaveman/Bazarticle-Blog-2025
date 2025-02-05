f21136bf752de912bc6a860dfa89fff0848b1f44
Evan Mastrantuono <xnaveman@gmail.com>
2025-02-05 11:46:13 +0100

    debug

--- config.php
+++ config.php
@@ -5,7 +5,9 @@ define('ROOT_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
 //Load env
 require_once ROOT . '/includes/libs/DotEnv.php';
-//(new DotEnv(ROOT.'/.env'))->load();
+if (strpos($_SERVER['HTTP_HOST'], 'scalingo') == false) {
+    (new DotEnv(ROOT.'/.env'))->load();
+}
 
 //defines
 require_once ROOT . '/config/defines.php';
