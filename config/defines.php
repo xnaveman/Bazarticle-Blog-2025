bf930495f9d426cdc6d594b9ca9647c9832ace9c
Evan Mastrantuono <xnaveman@gmail.com>
2025-02-05 11:00:27 +0100

    setting up port

--- config/defines.php
+++ config/defines.php
@@ -4,6 +4,7 @@ if (strpos($_SERVER['HTTP_HOST'], 'scalingo') === false) {
     define('SQL_USER', getenv('SCALINGO_DB_USER'));
     define('SQL_PWD', getenv('SCALINGO_DB_PASSWORD'));
     define('SQL_DB', getenv('SCALINGO_DB_DATABASE'));
+    define('SQL_PORT', getenv('SCALINGO_DB_PORT'));
 } else {
     define('SQL_HOST', getenv('DB_HOST'));
     define('SQL_USER', getenv('DB_USER'));
--- functions/query/connect.php
+++ functions/query/connect.php
@@ -3,7 +3,9 @@
 function sql_connect(){
     global $DB;
 
-    //connect BDD with PDO using SQL_HOST, SQL_USER, SQL_PWD, SQL_DB
-    // Avec encodage UTF8
-    $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB, SQL_USER, SQL_PWD);
+    if (strpos($_SERVER['HTTP_HOST'], 'scalingo') === false) {
+        $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB . ';ports=' . SQL_PORT, SQL_USER, SQL_PWD);
+    } else {
+        $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB, SQL_USER, SQL_PWD);
+}
 }
