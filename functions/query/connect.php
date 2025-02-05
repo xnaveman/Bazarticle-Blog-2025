df991102cc2977b785c16556ada604a046dbe053
Evan Mastrantuono <xnaveman@gmail.com>
2025-02-05 11:25:02 +0100

    fix bad mysql string

--- functions/query/connect.php
+++ functions/query/connect.php
@@ -4,7 +4,7 @@ function sql_connect(){
     global $DB;
 
     if (strpos($_SERVER['HTTP_HOST'], 'scalingo') === false) {
-        $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB . ';ports=' . SQL_PORT, SQL_USER, SQL_PWD);
+        $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB . ';port=' . SQL_PORT, SQL_USER, SQL_PWD);
     } else {
         $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB, SQL_USER, SQL_PWD);
 }
