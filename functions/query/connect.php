51a2e91a64e2a2e3b8fbd0bbbe848d4f2fed7914
Evan Mastrantuono <xnaveman@gmail.com>
2025-02-05 11:35:23 +0100

    debug

--- functions/query/connect.php
+++ functions/query/connect.php
@@ -3,6 +3,11 @@
 function sql_connect(){
     global $DB;
 
+    var_dump(SQL_DB);
+    var_dump(SQL_USER);
+    var_dump(SQL_PWD);
+    var_dump(SQL_HOST);
+    var_dump(SQL_PORT);
     if (strpos($_SERVER['HTTP_HOST'], 'scalingo') !== false) {
         $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB . ';port=' . SQL_PORT, SQL_USER, SQL_PWD);
     } else {
