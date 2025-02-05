f1747b097570fee7758cdcb1b04d8591691149ce
Evan Mastrantuono <xnaveman@gmail.com>
2025-02-05 10:43:34 +0100

    db env update

--- config/defines.php
+++ config/defines.php
@@ -1,5 +1,12 @@
 <?php
-define('SQL_HOST', getenv('DB_HOST'));
-define('SQL_USER', getenv('DB_USER'));
-define('SQL_PWD', getenv('DB_PASSWORD'));
-define('SQL_DB', getenv('DB_DATABASE'));
+if (strpos($_SERVER['HTTP_HOST'], 'scalingo') === false) {
+    define('SQL_HOST', getenv('SCALINGO_DB_HOST'));
+    define('SQL_USER', getenv('SCALINGO_DB_USER'));
+    define('SQL_PWD', getenv('SCALINGO_DB_PASSWORD'));
+    define('SQL_DB', getenv('SCALINGO_DB_DATABASE'));
+} else {
+    define('SQL_HOST', getenv('DB_HOST'));
+    define('SQL_USER', getenv('DB_USER'));
+    define('SQL_PWD', getenv('DB_PASSWORD'));
+    define('SQL_DB', getenv('DB_DATABASE'));
+}
