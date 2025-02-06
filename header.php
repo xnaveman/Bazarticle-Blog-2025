ee36de66e15c9c65dbb53b99258f44c0c2bbff99
Evan Mastrantuono <xnaveman@gmail.com>
2025-02-06 09:38:05 +0100

    css changes

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Bordeaux : Le monde à travers Bordeaux</title>
   <link rel="icon" type="image/x-icon" href="front/assets/favicon.ico" />
-  <link href="/front/CSS/styles.css" rel="stylesheet" />
+  <link href="front/css/styles.css" rel="stylesheet" />
   <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
