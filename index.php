98de0dee16a2f9681a1e1542fe451c46d908dccc
EricTrky <erictranky@gmail.com>
2025-02-07 02:15:08 +0100

    Affichage mot cle

--- index.php
+++ index.php
@@ -11,13 +11,22 @@ $articlesrecents=sql_select("article", "*",null,null,'numArt DESC','2');
 
 <?php 
 foreach ($articlesrecents as $key => $article) {
+    $motclesarticles=sql_select("motclearticle","*","numArt=".$article['numArt']);
+    foreach ($motclesarticles as $key => $motclesarticle) {
+        $motcles[]=sql_select("motcle","*","numMotcle=".$motclesarticle['numMotCle'])[0];
+    }
     $thematiques=sql_select("thematique","*","numThem=".$article['numThem']);
     echo '<div class="post-preview">
             <a href="article.php?numArt='.$article['numArt'].'">
                 <h2 class="post-title">'.$thematiques[0]['libThem'].'</h2>
                 <h3 class="post-subtitle">'.$article['libTitrArt'].'</h3>
-            </a>
-        </div>
+                <p class="post-subtitle">'.$article['libChapoArt'].'</p>
+            </a>';
+            foreach ($motcles as $key => $motcle) {
+                echo '<span class="badge rounded-pill bg-dark" style="margin-left: 5px; margin-right: 5px;">'.$motcle['libMotCle'].'</span>';
+            }
+            unset($motcles);
+        echo '</div>
         <hr class="my-4" />';
 }
 ?>
