a188f4ba2c0929f7dfb4ee2c7faedd145c8e9fac
EricTrky <erictranky@gmail.com>
2025-02-07 02:17:12 +0100

    Affichage mot cle dans recherche

--- recherche.php
+++ recherche.php
@@ -65,10 +65,18 @@ if (isset($_POST)){
                 <div class="col-md-10 col-lg-8 col-xl-7">
 <?php
 foreach ($articles as $key => $article) {
+	$motclesarticles=sql_select("motclearticle","*","numArt=".$article['numArt']);
+                        foreach ($motclesarticles as $key => $motclesarticle) {
+                            $motcles[]=sql_select("motcle","*","numMotcle=".$motclesarticle['numMotCle'])[0];
+                        }
                         echo '<a href="Article.php?numArt='.$article['numArt'].'">
                         <h4 class="article-title">'.$article['libTitrArt'].'</h4>
                         <p class="post-subtitle">'.$article['libChapoArt'].'</p>
                     </a>';
+					foreach ($motcles as $key => $motcle) {
+						echo '<span class="badge rounded-pill bg-dark" style="margin-left: 5px; margin-right: 5px;">'.$motcle['libMotCle'].'</span>';
+					}
+					unset($motcles);
                     echo '<hr class="my-4" />';
 }
 ?>
